567ace6f4b59c6c5eb7c354e834e0c067b327229
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2020-12-13 01:30:10 +0600

    BT-327

--- src/aws_lambda_layers/databases.py
+++ src/aws_lambda_layers/databases.py
@@ -16,6 +16,7 @@ def create_cassandra_connection(db_username, db_password, db_host, db_port, db_l
     auth_provider = PlainTextAuthProvider(username=db_username, password=db_password)
     default_profile = ExecutionProfile(
         load_balancing_policy=DCAwareRoundRobinPolicy(local_dc=db_local_dc),
+        default_retry_policy=RetryPolicy(),
         consistency_level=ConsistencyLevel.LOCAL_QUORUM,
         request_timeout=60,
         row_factory=dict_factory
@@ -28,8 +29,7 @@ def create_cassandra_connection(db_username, db_password, db_host, db_port, db_l
         protocol_version=4,
         connect_timeout=60,
         idle_heartbeat_interval=0,
-        execution_profiles={EXEC_PROFILE_DEFAULT: default_profile},
-        default_retry_policy=RetryPolicy()
+        execution_profiles={EXEC_PROFILE_DEFAULT: default_profile}
     )
     connection = cluster.connect()
     return connection
